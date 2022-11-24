99581895f7800c084c2046bd647caaf0d069d467
Brian Zill <bzill@microsoft.com>
2022-11-24 04:20:00 +0000

    Switched to new Rust API and added Connection type.

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,193 @@
-use ::demikernel::{LibOS, OperationResult, QDesc, QToken};
-use demikernel::LibOSName;
-use std::fs;
-use ::std::net::{Ipv4Addr, SocketAddrV4};
-// use ::runtime::memory::Buffer;
-use demikernel::runtime::memory::Buffer;
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// A (Very) Simple HTTP Server.
+
+use ::demikernel::{
+    demi_sgarray_t,
+    runtime::{
+        fail::Fail,
+        types::demi_opcode_t,
+    },
+    LibOS,
+    LibOSName,
+    QDesc,
+    QToken,
+};
+use ::std::{
+    collections::{
+        HashMap,
+    },
+    fs,
+    io::Write,
+    net::{
+        Ipv4Addr,
+        SocketAddrV4,
+    },
+    slice,
+};
 
 fn main() -> ! {
-    let libos_name: String = "catnip".to_string();
-
-    let libos = match LibOS::new(LibOSName::Catnip){
-        Ok(libs) => libs,
-        Err(e) => {
-            println!("Error: {e}");
-        }
+    // Pull LibOS from environment variable "LIBOS" if present.  Otherwise, default to Catnap.
+    let libos_name: LibOSName = match LibOSName::from_env() {
+        Ok(libos_name) => libos_name.into(),
+        Err(_) => LibOSName::Catnap,
     };
 
-    // Create listening socket
-    let local = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 7878);
-    let sockqd: QDesc = match libos.socket(libc::AF_INET, libc::SOCK_STREAM, 0) {
+    // Initialize the LibOS.
+    let mut libos: LibOS = match LibOS::new(libos_name) {
+        Ok(libos) => libos,
+        Err(e) => panic!("failed to initialize libos: {:?}", e.cause),
+    };
+
+    // Create listening socket (a QDesc in Demikernel).
+    let local_addr: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 7878);
+    let listening_qd: QDesc = match create_listening_socket(&mut libos, local_addr) {
         Ok(qd) => qd,
-        Err(e) => panic!("failed to create socket: {:?}", e.cause),
+        Err(e) => panic!("create_listening_socket failed: {:?}", e.cause),
     };
 
-    // Bind to local address
-    match libos.bind(sockqd, local) {
-        Ok(()) => (),
-        Err(e) => panic!("failed to bind socket: {:?}", e.cause),
-    };
+    println!("Listening on local address: {:?}", local_addr);
 
-    // Mark socket as a passive one.
-    match libos.listen(sockqd, 16) {
-        Ok(()) => (),
-        Err(e) => panic!("failed to listen socket: {:?}", e.cause),
-    }
+    // Create list of queue tokens (QToken) representing operations we're going to wait on.
+    let mut waiters: Vec<QToken> = Vec::new();
 
-    println!("Local Address: {:?}", local);
-        
-    // create list of qtokens
-    let mut qtokens: Vec<QToken> = Vec::new();
-
-    // Wait for first connection
-    let accept_qt: QToken = match libos.accept(sockqd) {
+    // Post an accept for the first connection.
+    let accept_qt: QToken = match libos.accept(listening_qd) {
         Ok(qt) => qt,
         Err(e) => panic!("failed to accept connection on socket: {:?}", e.cause),
     };
-    qtokens.push(accept_qt);
+    // Add this accept to the list of operations we're waiting to complete.
+    waiters.push(accept_qt);
 
-    // loop over connections
+    // Create hash table of accepted connections.
+    let mut connections: HashMap<QDesc, Connection> = HashMap::new();
+
+    // Loop over queue tokens we're waiting on.
     loop {
-        let (i, qd, result) = match libos.wait_any2(&qtokens) {
-            Ok((i, qd, result)) => (i, qd, result),
+        let (index, qr) = match libos.wait_any(&waiters, None) {
+            Ok((i, qr)) => (i, qr),
             Err(e) => panic!("Wait failed: {:?}", e),
         };
-        
-        let qt = match result {
-            // New connection
-            OperationResult::Accept(qd) => {
-                // pop from new connection
-                let qt: QToken = match libos.pop(qd) {
-                    Ok(qt) => qt,
+
+        // Since this QToken completed, remove it from the list of waiters.
+        waiters.swap_remove(index);
+
+        // Find out what operation completed:
+        match qr.qr_opcode {
+            demi_opcode_t::DEMI_OPC_ACCEPT => {
+                // A new connection arrived.
+                let qd: QDesc = unsafe { qr.qr_value.ares.qd.into() };
+                println!("Connection established!  Queue Descriptor = {:?}", qd);
+
+                // Create new connection state and store it.
+                let connection: Connection = Connection::new(qd);
+                connections.insert(qd, connection);
+
+                // Post a pop from the new connection.
+                match libos.pop(qd) {
+                    Ok(qt) => waiters.push(qt),
                     Err(e) => panic!("failed to pop data from socket: {:?}", e.cause),
                 };
-                qtokens.push(qt);
-                println!("Connection established!");
-                // accept next connection
-                match libos.accept(sockqd) {
-                    Ok(qt) => qt,
+
+                // Post an accept for the next connection.
+                match libos.accept(listening_qd) {
+                    Ok(qt) => waiters.push(qt),
                     Err(e) => panic!("failed to accept connection on socket: {:?}", e.cause),
                 }
-            }
-            // Pop
-            OperationResult::Pop(_, buf) => {
-                // process buffer
-                let response = process_request(buf);
-                match libos.push2(qd, &response) {
-                    Ok(qt) => qt,
-                    Err(e) => panic!("failed to push data to socket: {:?}", e.cause),
-                }       
-                // match libos.wait2(qt) {
-                //     Ok((_, OperationResult::Push)) => (),
-                //     Err(e) => panic!("Push response failed: {:?}", e.cause),
-                // };
-            }
-            // Push
-            OperationResult::Push => {
-                // pop again
+            },
+            demi_opcode_t::DEMI_OPC_POP => {
+                // A pop completed.
+                let qd: QDesc = qr.qr_qd.into();
+                let recv_sga: demi_sgarray_t = unsafe { qr.qr_value.sga };
+
+                // Find Connection for this queue descriptor.
+                let connection: &mut Connection = connections.get_mut(&qd).expect("HashMap should hold connection!");
+
+                // Process the incoming request.
+                let oqt: Option<QToken> = match connection.process_data(&mut libos, recv_sga) {
+                    Ok(oqt) => oqt,
+                    Err(e) => panic!("process data failed: {:?}", e.cause),
+                };
+
+                if oqt.is_some() {
+                    waiters.push(oqt.expect("oqt should be some!"));
+                }
+
+                // Post another pop.
                 match libos.pop(qd) {
-                    Ok(qt) => qt,
+                    Ok(qt) => waiters.push(qt),
                     Err(e) => panic!("failed to pop data from socket: {:?}", e.cause),
                 }
-            }
-            OperationResult::Failed(e) => panic!("operation failed: {:?}", e),
-            _ => panic!("unexpected result"),
-        };
+            },
+            demi_opcode_t::DEMI_OPC_PUSH => {
+                // A push completed.
+                let _qd: QDesc = qr.qr_qd.into();
 
-        qtokens[i] = qt;
+                // ToDo: Can free the sga now.
+            },
+            demi_opcode_t::DEMI_OPC_FAILED => panic!("operation failed"),
+            _ => panic!("unexpected opcode"),
+        };
     }
 }
 
-fn process_request(buffer: Box<dyn Buffer>) -> Vec<u8> {
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let contents = fs::read_to_string("hello.html").unwrap();
+// Creates a listening socket with the given IPv4 address/port and returns the queue descriptor for it.
+fn create_listening_socket(libos: &mut LibOS, address: SocketAddrV4) -> Result<QDesc, Fail> {
+    // Create the socket.
+    let listening_qd: QDesc = libos.socket(libc::AF_INET, libc::SOCK_STREAM, 0)?;
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
-    response.as_bytes().to_vec()
+    // Bind to local address.
+    libos.bind(listening_qd, address)?;
+
+    // Mark socket as a passive (i.e. listening) one.
+    const BACKLOG: usize = 16;
+    libos.listen(listening_qd, BACKLOG)?;
+
+    Ok(listening_qd)
+}
+
+// The Connection type tracks connection state.
+struct Connection {
+    queue_descriptor: QDesc,
+    receive_queue: Vec<demi_sgarray_t>,
+}
+
+impl Connection {
+    pub fn new(qd: QDesc) -> Self {
+        Connection {
+            queue_descriptor: qd,
+            receive_queue: Vec::new(),
+        }
+    }
+
+    pub fn process_data(&mut self, libos: &mut LibOS, rsga: demi_sgarray_t) -> Result<Option<QToken>, Fail> {
+        // We only handle single-segment scatter-gather arrays for now.
+        assert_eq!(rsga.sga_numsegs, 1);
+
+        // Print the incoming data.
+        let slice: &mut [u8] = unsafe { slice::from_raw_parts_mut(rsga.sga_segs[0].sgaseg_buf as *mut u8, rsga.sga_segs[0].sgaseg_len as usize) };
+        println!("Received: {}", String::from_utf8_lossy(slice));
+
+        // Add incoming scatter-gather segment to our receive queue.
+        self.receive_queue.push(rsga);
+
+        // Craft a response and send it.
+        let ssga: demi_sgarray_t = libos.sgaalloc(1500)?;
+        let mut slice: &mut [u8] = unsafe { slice::from_raw_parts_mut(ssga.sga_segs[0].sgaseg_buf as *mut u8, ssga.sga_segs[0].sgaseg_len as usize) };
+
+        // Read the entire file into a string.
+        let contents: String = fs::read_to_string("hello.html").expect("file should exist and be readable.");
+
+        write!(slice,
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        )?;
+
+        // Write response.
+        let qt = libos.push(self.queue_descriptor, &ssga)?;
+
+        Ok(Some(qt))
+    }
 }
